b9673a0770e3a06a4fd017fac010b38d3c948529
thomas <thomas.chemineau@gmail.com>
2012-09-28 15:27:00 +0200

    add lemonldap options for this module

--- config/lemonldap.php
+++ config/lemonldap.php
@@ -2,5 +2,41 @@
 
 return array
 (
+
+	// Security parameters
+	'security' => array (
+
+		// IP address of the Lemonldap::NG server. The IP address into
+		// the HTTP header named REMOTE_IP will be verified before
+		// authenticating the user.
+		// Set FALSE to disable this check
+		'server_ip' => '127.0.0.1',
+
+		// Name of the HTTP header that specify the security token to verify the
+		// identity of the request
+		// Set FALSE to disable this check
+		'token_header_name' => 'HTTP_AUTH_TOKEN',
+
+		// Token value
+		// Only verify if token_header_name is specified
+		'token_header_value' => '123456',
+
+	),
+
+	// Here, define mapping between internal variables and HTTP headers. Add your own
+	// internal variables which will be stored in session and could be retrieve by
+	// calling $auth->get_user()
+	'mapping' => array (
+
+		// Default values for user mapping
+		'user' => array (
+
+			'username' => 'HTTP_AUTH_USER',
+			'mail' => 'HTTP_AUTH_MAIL',
+
+		),
+
+	),
+
 );
 
