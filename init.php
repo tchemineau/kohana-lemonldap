4bd33f00f2f6b42d4de226c1c50f868ac270573e
thomas <thomas.chemineau@gmail.com>
2012-11-30 14:14:53 +0100

    add init file

---
+++ init.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+/**
+ * Include class to retrieve Cookies from SOAP response.
+ */
+require_once(dirname(__FILE__).'/vendor/CookiesResponse.class.php');
+
